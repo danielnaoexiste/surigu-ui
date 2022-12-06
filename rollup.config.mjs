45e92bbb4c9b5456fb5e3a368e9e8c92216019b5
danielnaoexiste <danieldenardo1@gmail.com>
2022-12-06 18:10:08 -0300

    feat: adds css support

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
-const packageJson = require("./package.json");
+import postcss from "rollup-plugin-postcss";
+import packageJson from "./package.json" assert { type: "json" };
 
 export default [
   {
@@ -24,11 +25,13 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.css$/],
   },
 ];
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,3 +1,5 @@
+import "./Button.css";
+
 export interface ButtonProps {
   label: string;
 }
